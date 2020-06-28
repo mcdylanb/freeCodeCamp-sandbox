ec3913751b8ed144a4dfe9732679f7ff11bae97b
mcdylanb <dylansatgalab@gmail.com>
2020-06-28 15:42:07 +0800

    using newStr and for loop

--- Basic-alogrithms/repeatStringNumTimes.js
+++ Basic-alogrithms/repeatStringNumTimes.js
@@ -1,11 +1,18 @@
 const repeatStringNumTimes = (str, num) => {
+  let newStr = "";
   if (num < 0) {
     str = "";
   } else {
-    str = str.repeat(num);
+    //iterate each number
+    for (let x = 1; x <= num; x++) {
+      //then runs this code adding the str to the newStr
+      newStr += str;
+    }
+    //changes str into newStr
+    str = newStr;
   }
   return str;
 };
 
 console.log(repeatStringNumTimes("*", 10));
-console.log(repeatStringNumTimes("abc", -3));
+// console.log(repeatStringNumTimes("abc", -3));
